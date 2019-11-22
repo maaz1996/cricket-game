2d1270622bb465b2bb7ba80288ba4350e3203310
maaz1996 <maaz.abdullah111@gmail.com>
2019-11-22 11:12:45 +0530

    -added python structure; random no. generated

--- game.cpp
+++ game.cpp
@@ -1,25 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-	string team1,team2;
-	cout<<"Select team1_";
-	cin>>team1;
-	cout<<"Select team2_";
-	cin>>team2;
+	string team1, team2;
+	cout << "Select team1_";
+	cin >> team1;
+	cout << "Select team2_";
+	cin >> team2;
 	int overs;
-	cout<<"Enter the no. of Overs to be Played in this Match between "<< team1<<" and "<<team2<<endl;
-	cin>>overs;
-	int balls=overs*6;
-	cout<<"There will be "<< overs<<" overs and "<<balls<<" balls in the Match between "<<team1<<" and "<<team2<<endl;
-	int runs=0;
-	int wickets=0;
-	 
-	for(int i=0;i<balls;i++)
-	{//generating a random no. from 1 to 6
-		int r=1+rand()%5;
-		cout<<r<<endl;
+	cout << "Enter the no. of Overs to be Played in this Match between " << team1 << " and " << team2 << endl;
+	cin >> overs;
+	int balls = overs * 6;
+	cout << "There will be " << overs << " overs and " << balls << " balls in the Match between " << team1 << " and " << team2 << endl;
+	int runs = 0;
+	int wickets = 0;
+
+	for (int i = 0; i < balls; i++)
+	{ //generating a random no. from 1 to 6
+		int r = 1 + rand() % 5;
+		cout << r << endl;
 	}
 	return 0;
 }
--- game.py
+++ game.py
@@ -1,11 +1,19 @@
 from random import randint
 
+team1 = input("Enter Team1_")
+team2 = input("Enter Team2_")
 overs = input("How many overs? ")
+
+balls = int(overs)*6
 
 
 def start(overs):
-    balls = overs*6
-    print"There will be %d overs and %d balls"(overs, balls)
+    print("There will be ", overs, " overs and ", balls,
+          " balls between ", team1, " and ", team2)
 
 
 start(overs)
+
+for i in range(balls):
+    r = randint(1, 6)
+    print(r)
